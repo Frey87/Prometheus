44b1f5133ac9392e827cb3968558073a63be45b5
Frey87 <v.veryovkin@gmail.com>
2024-12-09 22:10:23 +0200

    Update test_github_api.py

--- QA_Auto_GlobalLogic/Frameworks/tests/api/test_github_api.py
+++ QA_Auto_GlobalLogic/Frameworks/tests/api/test_github_api.py
@@ -8,3 +8,8 @@ def test_user_exists():
   user = api.get_user_defunkt()
   assert user['login'] == 'defunkt'
   
+@pytest.mark.api
+def test_user_not_exists():
+  api = GitHub()
+  r = api.get_non_exist_user()
+  assert r['message'] == 'Not Found'
