288f847505c72de99c53a00a67e6e1f4a4a1bfb9
Frey87 <v.veryovkin@gmail.com>
2024-12-11 20:45:30 +0200

    Update database.py

--- QA_Auto_GlobalLogic/Frameworks/moules/common/database.py
+++ QA_Auto_GlobalLogic/Frameworks/moules/common/database.py
@@ -1,1 +1,14 @@
+import sqlite3
 
+
+class Database():
+  def __init__(self):
+    self.connection = sqlite3.connect('insert path to database'+'name of your database')
+    self.cursor = self.connection.cursor()
+
+  
+  def test_connection(self):
+    sqlite_select_Query = "SELECT sqlite_version();"
+    self.cursor.execute(sqlite_select_Query)
+    record = self.cursor.fetchall()
+    print(f"Connected successfully. SQLite Database Version is: {record}")
