e92cb3db8da7a9c55481b3a936f697f458a8c474
Frey87 <v.veryovkin@gmail.com>
2024-12-17 22:20:23 +0200

    Update sign_in_page.py

--- QA_Auto_GlobalLogic/Frameworks/tests/ui/page_objects/sign_in_page.py
+++ QA_Auto_GlobalLogic/Frameworks/tests/ui/page_objects/sign_in_page.py
@@ -1,1 +1,35 @@
+from moules.ui.page_objects.base_page importBasePage
+frome selenium.webdriver.common.by import By
 
+
+Class SignInPage(BasePage):
+    URL = "https://github.com/login"
+
+    def __init__(self) -> None:
+      super().__init__()
+      
+    def go_to(self):
+      self.driver.get(SignInPage.URL)
+
+    def try_login(self, username, password):
+      
+      login_elem = self.driver.find_element(By.ID, "login_fieled")
+
+      
+      login_elem.send_keys(username)
+
+      
+      pass_elem = self.driver.find_element(By.ID, "password")
+
+      
+      pass_elem.send_keys(password)
+
+      
+      btn_elem = self.driver.find_element(By.NAME, "commit")
+
+      
+      btn_elem.click()
+
+
+    def check_title(self, expected_title):
+      return self.driver.title == expected_title
